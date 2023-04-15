c09663ee4dbab4dcdc3347910ca10abd52b6e630
iberis2 <snow9402@gmail.com>
2023-04-15 10:02:09 +0900

    refactor: switch/case문, string->변수로 변경

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+// stirng으로  "add" 쓰면 오타났을 때 뭐가 잘 못된지 모르는데, 변수로 쓰면 오타나면 자바스크립트에서 선언되지 않은 변수라고 알려줌
+const ADD = "add";
+const MINUS = "minus";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "Add") {
-    return count + 1;
-  } else if (action.type === "Minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return ++count;
+    case MINUS:
+      return --count;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
+console.log(countStore);
 
 const onChange = () => {
   number.innerText = countStore.getState();
@@ -23,6 +29,6 @@ const onChange = () => {
 
 countStore.subscribe(onChange);
 
-add.addEventListener("click", () => countStore.dispatch({ type: "Add" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
 
-minus.addEventListener("click", () => countStore.dispatch({ type: "Minus" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
