8dcbf0c2935903439f4c2b00b1e30ab119c1ff53
iberis2 <snow9402@gmail.com>
2023-04-15 14:00:04 +0900

    feat: react-redux 기본 셋팅

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = (text) => {
+  return { type: ADD, text };
+};
+
+export const deleteToDo = (id) => {
+  return { type: DELETE, id };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE:
+      return state.filter((todo) => todo.id !== action.id);
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+// store.dispatch(addToDo);
+// store.dispatch(deleteToDo);
+
+export default store;
