fb60cab3310c8c7c35700b760bcf0d7cab1e5ebb
iberis2 <snow9402@gmail.com>
2023-04-15 09:12:17 +0900

    vanilla redux 로 count +/-

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "Add") {
+    return count + 1;
+  } else if (action.type === "Minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: "Add" }));
+
+minus.addEventListener("click", () => countStore.dispatch({ type: "Minus" }));
