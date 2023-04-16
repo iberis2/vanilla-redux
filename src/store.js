7cb2f017b2178ad072667112144e0862d551276d
iberis2 <snow9402@gmail.com>
2023-04-16 11:08:32 +0900

    refact: redux-toolkit createReducer으로 switch/case 문 없이수정, mutate로 state 바꾸기(state에 직접 수정(push 등) 가능

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DELETE = "DELETE";
@@ -7,16 +7,26 @@ const DELETE = "DELETE";
 const addToDo = createAction(ADD);
 const deleteToDo = createAction(DELETE);
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [...state, { text: action.payload, id: Date.now() }];
-    case deleteToDo.type:
-      return state.filter((todo) => todo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [...state, { text: action.payload, id: Date.now() }];
+//     case deleteToDo.type:
+//       return state.filter((todo) => todo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    return state.filter((todo) => todo.id !== action.payload);
+  },
+});
+
 const store = createStore(reducer);
 
 export const actionCreators = {
